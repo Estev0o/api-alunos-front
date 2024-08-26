5d585655ada83bfee0092a44de5cb3e7bedf3096
Vinicius Estevo <viniestevo33@gmail.com>
2024-08-25 21:20:27 -0300

    add shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,8 @@ export default {
   theme: {
     extend: {},
   },
+  boxShadow: {
+    shape: '8px 8px 8px rgba(0,0,0,0.13), 8px 4px 4px rgba(0,0,0,0.1), 8px 2px 2px rgba(0,0,0,0.1), 8px 8px 8px 1px rgba(0,0,0,0.1), inset 0px 0px 0px 1px rgba(255,255,255,0.3), inset 0px 1px 0px rgba(255,255,255,0.3)' ,
+  },
   plugins: [],
 }
