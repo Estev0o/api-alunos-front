443069450b0bf5e633e9ac7a45870161d0df99ae
Vinicius Estevo <viniestevo33@gmail.com>
2024-08-25 21:20:54 -0300

    imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Table from './components/table/table'
+import Create from './components/modalCreate/create'
 import './App.css'
 
 function App() {
   
   return (
     <>
-      
+      <Table></Table>
+      <Create/>
     </>
   )
 }
