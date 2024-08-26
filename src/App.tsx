847ac99451d0e00bfc0a2f575ddda80eeb3e9df4
Vinicius Estevo <viniestevo33@gmail.com>
2024-08-25 21:51:49 -0300

    start using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import { useState } from 'react'
 import Table from './components/table/table'
-import Create from './components/modalCreate/create'
+import CreateModal from './components/modalCreate/create'
 import './App.css'
 
 function App() {
-  
+  const [isModalCreateOpen, setModalCreateOpen] = useState(false)
+
+  function openModalCreate() {
+    setModalCreateOpen(true)
+  }
+
+  function closeModalCreate() {
+    setModalCreateOpen(false)
+  }
+
   return (
     <>
-      <Table></Table>
-      <Create/>
+      {isModalCreateOpen && (
+        <CreateModal
+          closeModalCreate={closeModalCreate}
+        />
+      )}
     </>
   )
 }
 
 export default App
+////</Table>
