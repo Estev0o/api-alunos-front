adf80d1d148eb4b7465642afab3b6cbf912e7757
Vinicius Estevo <viniestevo33@gmail.com>
2024-08-27 21:43:36 -0300

    conection ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,35 @@
-import { useState, useEffect } from 'react'
-//import Table from './components/table/table'
-//import CreateModal from './components/modalCreate/create'
-import api from './services/api'
-import './App.css'
+import { useState, useEffect } from 'react';
+import api from './services/api';
+import './App.css';
+
+// Defina a interface que representa a estrutura do objeto mensagem
+interface Mensagem {
+  id: number;     // ou string, dependendo do tipo no banco de dados
+  nome: string;   // outras propriedades que você queira incluir
+}
 
 export default function App() {
-  //const [isModalCreateOpen, setModalCreateOpen] = useState(false)
+  // Tipagem do estado com a interface Mensagem[]
+  const [mensagens, setMessages] = useState<Mensagem[]>([]);
 
-  //function openModalCreate() {
-    //setModalCreateOpen(true)
-  //}
-
-  //function closeModalCreate() {
-    //setModalCreateOpen(false)
-  //}
-
-  const [mensagens, setMessages] = useState([])
   useEffect(() => {
-    api.post('List').then(({data}) => {
-      setMessages(data)
-    })
-  })
+    api.get<Mensagem[]>('api/Alunos/List')  // Certifique-se de que a rota está correta
+      .then(({ data }) => {
+        setMessages(data);
+      })
+      .catch(error => {
+        console.error("Erro ao buscar mensagens:", error);
+      });
+  }, []);
 
   return (
     <>
-
-    {mensagens.map(item => {
-      <div key={item.id}></div>
-    })}
-      
+      {mensagens.map((item) => (
+        <div key={item.id}>
+          {item.id}
+          {item.nome} {/* ou qualquer outra propriedade que você queira exibir */}
+        </div>
+      ))}
     </>
-  )
+  );
 }
-
-
-////</Table>
-///{isModalCreateOpen && (
- // <CreateModal
- // //  closeModalCreate={closeModalCreate}
-  ///>
-//)}
