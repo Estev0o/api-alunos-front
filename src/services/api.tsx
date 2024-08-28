05414d482debb166f04632438d393a83ec18af85
Vinicius Estevo <viniestevo33@gmail.com>
2024-08-27 21:43:23 -0300

    api ok

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://localhost:44315/api/Alunos/",
+    baseURL: "https://localhost:44315/",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 export default api
