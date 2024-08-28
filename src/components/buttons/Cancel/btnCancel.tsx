e423516f45cf58f70d9b9e0c890033e5c5cc39c1
vinicius <viniestevo33@gmail.com>
2024-08-28 14:17:32 -0300

    new customization

--- src/components/buttons/Cancel/btnCancel.tsx
+++ src/components/buttons/Cancel/btnCancel.tsx
@@ -3,7 +3,7 @@ import { Ban } from "lucide-react";
 export default function BtnCancel() {
     return (
         <>
-            <button className="bg-rose-900 text-zinc-50 text-xl py-2 px-6 rounded-xl shadow-md hover:bg-pink-600 flex items-center">
+            <button className="bg-rose-900 text-zinc-50 text-xl py-2 px-6 rounded-xl shadow-md hover:bg-pink-600 flex items-center w-full justify-center">
                 Cancelar
                 <Ban className='ml-3 size-5 text-zinc-50'/>
             </button>
--- src/components/buttons/create/btnCreate.tsx
+++ src/components/buttons/create/btnCreate.tsx
@@ -4,7 +4,7 @@ import { UserPlus } from "lucide-react";
 export default function BtnCreate() {
     return (
         <>
-            <button className="bg-cyan-900 text-zinc-50 text-xl py-2 px-6 rounded-xl shadow-md hover:bg-cyan-600 flex items-center">
+            <button className="bg-cyan-900 text-zinc-50 text-xl py-2 px-6 rounded-xl shadow-md hover:bg-cyan-600 flex items-center w-full justify-center">
                 Incluir
                 <UserPlus className='ml-3 size-5 text-zinc-50' />
             </button>
