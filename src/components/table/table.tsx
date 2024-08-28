4ac289bb51e0781b3b933cd49a8b873dfa22ef70
vinicius <viniestevo33@gmail.com>
2024-08-28 16:50:24 -0300

    pre-set tables (start)

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,37 +1,52 @@
 import { } from "lucide-react"
-import { useState } from "react"
+import { useState, useEffect} from "react"
+import api from "../../services/api";
+
+interface DataTable {
+    id: number;     // ou string, dependendo do tipo no banco de dados
+    nome: string;   // outras propriedades que você queira incluir
+    idade: number;
+    email: string
+}
 
 export default function Table() {
+
+    const [dataTable, setdataTable] = useState<DataTable[]>([]);
+
+useEffect(() => {
+    api.get<DataTable[]>('List')  // Certifique-se de que a rota está correta
+        .then(({ data }) => {
+            setdataTable(data);
+        })
+        .catch(error => {
+            console.error("Erro ao buscar mensagens:", error);
+        });
+}, []);
     return (
         <div className="h-screen flex flex-col justify-start gap-x-10 p-6">
             <div className="w-full flex justify-center">
-                <button  className="text-zinc-50 text-2xl flex mb-3 px-10">Incuir aluno</button>
+                <button className="text-zinc-50 text-2xl flex mb-3 px-10">Incuir aluno</button>
             </div>
-            
+
             <table className="table-fixed w-full border-collapse border border-gray-200">
                 <thead>
                     <tr className="bg-sky-800 text-white text-xl">
-                        <th className="py-3 px-4">Song</th>
-                        <th className="py-3 px-4">Artist</th>
-                        <th className="py-3 px-4">Year</th>
+                        <th className="py-3 px-4">ID</th>
+                        <th className="py-3 px-4">Nome</th>
+                        <th className="py-3 px-4">Idade</th>
+                        <th className="py-3 px-4">E-mail</th>
                     </tr>
                 </thead>
+
                 <tbody>
-                    <tr>
-                        <td className="py-3 px-4">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                        <td className="py-3 px-4">Malcolm Lockyer</td>
-                        <td className="py-3 px-4">1961</td>
-                    </tr>
-                    <tr>
-                        <td className="py-3 px-4">Witchy Woman</td>
-                        <td className="py-3 px-4">The Eagles</td>
-                        <td className="py-3 px-4">1972</td>
-                    </tr>
-                    <tr>
-                        <td className="py-3 px-4">Shining Star</td>
-                        <td className="py-3 px-4">Earth, Wind, and Fire</td>
-                        <td className="py-3 px-4">1975</td>
-                    </tr>
+                    {dataTable.map((itens) => (
+                        <tr>
+                            <td className="py-3 px-4"> {itens.id}</td>
+                            <td className="py-3 px-4"> {itens.nome}</td>
+                            <td className="py-3 px-4"> {itens.idade}</td>
+                            <td className="py-3 px-4"> {itens.email}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
