a5e41875b83bac1259d812de06a607205e083572
vinicius <viniestevo33@gmail.com>
2024-08-29 16:17:03 -0300

    API post working

--- src/components/modalCreate/create.tsx
+++ src/components/modalCreate/create.tsx
@@ -1,18 +1,47 @@
 import BtnCreate from "../buttons/create/btnCreate";
 import BtnCancel from "../buttons/Cancel/btnCancel";
+import { useState } from "react";
+import api from "../../services/api";
 
 interface CreateProps {
     closeModalCreate: () => void;
 }
 
 export default function CreateModal({ closeModalCreate }: CreateProps) {
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [idade, setIdade] = useState("");
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        // Valida se idade é um número
+        const idadeNumber = parseInt(idade, 10);
+        if (isNaN(idadeNumber)) {
+            console.error("Idade inválida");
+            return;
+        }
+
+        // Envia a solicitação POST para a API
+        api.post('CreateStudent', { Nome: nome, Email: email, Idade: idadeNumber })
+            .then(() => {
+                closeModalCreate();
+                setNome('');
+                setEmail('');
+                setIdade('');
+            })
+            .catch((error) => {
+                console.error('Erro ao enviar dados:', error.response?.data || error.message);
+            });
+    }
+
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-opacity-35 z-50 px-4">
             <div className="bg-zinc-400 rounded-xl bg-opacity-90 w-full max-w-lg p-6 max-h-screen overflow-auto transform">
                 <div className="flex items-center mb-4">
-                    <h3 className="text-zinc-50 text-2xl">Convide seus amigos e planeje sua próxima viagem!</h3>
+                    <h3 className="text-zinc-50 text-2xl">Adicionar Aluno</h3>
                 </div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="space-y-6">
                         <div className="flex flex-col space-y-4">
                             <div className="h-14 bg-zinc-800 px-4 rounded-xl flex items-center shadow-shape">
@@ -20,6 +49,8 @@ export default function CreateModal({ closeModalCreate }: CreateProps) {
                                     type="text"
                                     placeholder="Nome"
                                     className="bg-transparent text-lg placeholder-zinc-400 outline-none w-full"
+                                    value={nome}
+                                    onChange={(e) => setNome(e.target.value)}
                                 />
                             </div>
                             <div className="h-14 bg-zinc-800 px-4 rounded-xl flex items-center shadow-shape">
@@ -27,13 +58,17 @@ export default function CreateModal({ closeModalCreate }: CreateProps) {
                                     type="text"
                                     placeholder="Email"
                                     className="bg-transparent text-lg placeholder-zinc-400 outline-none w-full"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
                                 />
                             </div>
                             <div className="h-14 bg-zinc-800 px-4 rounded-xl flex items-center shadow-shape">
                                 <input
-                                    type="text"
+                                    type="number"
                                     placeholder="Idade"
                                     className="bg-transparent text-lg placeholder-zinc-400 outline-none w-full"
+                                    value={idade}
+                                    onChange={(e) => setIdade(e.target.value)}
                                 />
                             </div>
                         </div>
@@ -41,9 +76,8 @@ export default function CreateModal({ closeModalCreate }: CreateProps) {
                             <div className="flex-1">
                                 <BtnCreate />
                             </div>
-
                             <div className="flex-1">
-                                <BtnCancel />
+                                <BtnCancel onClick={closeModalCreate} />
                             </div>
                         </div>
                     </div>
