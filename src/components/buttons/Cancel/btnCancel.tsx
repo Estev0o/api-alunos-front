49baf2ff72337dad63a1d6520b37389a793eb1c3
vinicius <viniestevo33@gmail.com>
2024-08-29 16:16:50 -0300

    btn Cancel updated for close the modal

--- src/components/buttons/Cancel/btnCancel.tsx
+++ src/components/buttons/Cancel/btnCancel.tsx
@@ -1,12 +1,18 @@
 import { Ban } from "lucide-react";
 
-export default function BtnCancel() {
+interface BtnCancelProps {
+    onClick?: () => void;
+}
+
+export default function BtnCancel({ onClick }: BtnCancelProps) {
     return (
-        <>
-            <button className="bg-rose-900 text-zinc-50 text-xl py-2 px-6 rounded-xl shadow-md hover:bg-pink-600 flex items-center w-full justify-center">
-                Cancelar
-                <Ban className='ml-3 size-5 text-zinc-50'/>
-            </button>
-        </>
-    )
+        <button
+            onClick={onClick}
+            type="button"
+            className="bg-rose-900 text-zinc-50 text-xl py-2 px-6 rounded-xl shadow-md hover:bg-pink-600 flex items-center w-full justify-center"
+        >
+            Cancelar
+            <Ban className="ml-3 size-5 text-zinc-50" />
+        </button>
+    );
 }
