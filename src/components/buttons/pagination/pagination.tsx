e31ed9eb6b28dadfa4cce5f5fa6fda471cec0902
vinicius <viniestevo33@gmail.com>
2024-09-09 16:34:42 -0300

    pagination done

--- src/components/buttons/pagination/pagination.tsx
+++ src/components/buttons/pagination/pagination.tsx
@@ -4,25 +4,29 @@ interface PaginationProps {
     onPageChange: (page: number) => void;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
+const Pagination = ({ currentPage, totalPages, onPageChange }: PaginationProps) => {
+    const handlePrevPage = () => {
+        if (currentPage > 0) {
+            onPageChange(currentPage - 1);
+        }
+    };
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages - 1) {
+            onPageChange(currentPage + 1);
+        }
+    };
+
     return (
         <div className="flex justify-center mt-4">
-            <button
-                className="bg-cyan-900 text-zinc-50 rounded-xl hover:bg-cyan-600 px-4 py-2"
-                disabled={currentPage === 0}
-                onClick={() => onPageChange(currentPage - 1)}
-            >
+            <button onClick={handlePrevPage} disabled={currentPage === 0}>
                 Anterior
             </button>
-            <span className="mx-4 text-xl">
+            <span className="mx-4">
                 Página {currentPage + 1} de {totalPages}
             </span>
-            <button
-                className="bg-cyan-900 text-zinc-50 rounded-xl hover:bg-cyan-600 px-4 py-2"
-                disabled={currentPage >= totalPages - 1}
-                onClick={() => onPageChange(currentPage + 1)}
-            >
-                Próxima
+            <button onClick={handleNextPage} disabled={currentPage >= totalPages - 1}>
+                Próximo
             </button>
         </div>
     );
